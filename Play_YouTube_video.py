5aa71c7e2527a07a7b582ab81860f393bbec3a30
Abhishek Upadhyay <47971436+abhishekupadhyay260@users.noreply.github.com>
2021-08-09 01:04:21 +0530

    Add files via upload

---
+++ Play_YouTube_video.py
@@ -0,0 +1,6 @@
+# importing the module
+import pywhatkit
+
+YouTube_Video = input("enter YouTube video link \n") #to take url input from user
+pywhatkit.playonyt(YouTube_Video) #main program to play you tube video
+print("Playing...") #not must, i printed just to look cool
